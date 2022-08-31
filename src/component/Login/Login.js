1e51f6351ef5cad6eed1faef5d6611ab25be1cab
Habibur96 <habiburrahmannayan66@gmail.com>
2022-08-31 07:12:30 +0600

    SignUp form added

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -11,15 +11,15 @@ const Login = () => {
                 <form>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input type="email" name="email" id="" required />
                     </div>
 
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input type="password" name="password" id="" required />
                     </div>
 
-                    <input className='form-submit' type="submit" value="Login" />
+                    <input className='form-submit' type="submit" value="Login" required />
                 </form>
 
                 <p>New to Ema-John? <Link className='form-link' to='/SignUp'>Create an account</Link> </p>
